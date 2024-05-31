e7b74293fee98f67f484493152bc943c1761e6c6
Felix Hoffmann <hoffmannfelix.3@gmail.com>
2024-05-31 20:14:38 +0200

    overlay working

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -8,19 +8,13 @@ import styles from './Summary.module.css';
 const Summary = () => {
   const [showOverlay, setShowOverlay] = useState(false) //overlay false till button clicked 
   const [chooseMeal, setChooseMeal] = useState("noMeal")
-  
+  const [mainDish, setMainDish,] = useState("nothing")
 
-  const [mainDish, setMainDish] = useState({
-    meat: "no",
-    vegetarian: "no",
-    vegan: "no"
-  })
-  
 
   //make popup visible
   const mealSelected = (meal) => {
     setShowOverlay(true), // when true overlay is seen
-    setChooseMeal(meal)
+      setChooseMeal(meal)
   }
 
   //make popup visible
@@ -29,42 +23,55 @@ const Summary = () => {
   }
 
   //buttons to choose meat, vegetarian, vegan 
-const onOptionChange = (type) => (e) => {
-  //...mainDish copies array 
-  setMainDish({
-    ...mainDish,
-    [type]: e.target.value
-  })
-}
+  const onOptionChange = (e) => {
+    setMainDish(e.target.value)
+  }
 
 
-const onSubmit = (e) =>{
-  e.preventDefault()
-  const formData = new FormData(e.currentTarget)
-  const meal ={
-    name : formData.get('name'),
-    calories : formData.get('calories'),
-    carbohydrates : formData.get('carbohydrates'),
-    protein : formData.get('protein'),
-    fat : formData.get('fat'),
-    meat : mainDish.meat,
-    vegetarian : mainDish.vegetarian,
-    vegan : mainDish.vegan,
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const main = mainDish
+    console.log(main)
+    let meat1 = "no"
+    let vegetarian1 = "no"
+    let vegan1 = "no"
+    //finde the one thats picked 
+    if (main === 'meat') { meat1 = 'yes' }
+    else if (main === 'vegetarian') { vegetarian1 = 'yes' }
+    else if (main === 'vegan') { vegan1 = 'yes' }
+
+    const formData = new FormData(e.currentTarget)
+    const meal = {
+      name: formData.get('name'),
+      calories: formData.get('calories'),
+      carbohydrates: formData.get('carbohydrates'),
+      protein: formData.get('protein'),
+      fat: formData.get('fat'),
+      meat: meat1,
+      vegetarian: vegetarian1,
+      vegan: vegan1
+    }
+
+    console.log(meal.name)
+    console.log(meal.calories)
+    console.log(meal.carbohydrates)
+    console.log(meal.protein)
+    console.log(meal.fat)
+    console.log(meal.meat)
+    console.log(meal.vegetarian)
+    console.log(meal.vegan)
+
+    closePage();
   }
-  console.log(meal.name)
-  console.log(meal.calories)
-  console.log(meal.carbohydrates)
-  console.log(meal.fat)
-  console.log(meal.meat)
-  console.log(meal.vegetarian)
-  console.log(meal.vegan)
-}
-  
-const handleKeyPress = (e) => {
-  if (e.key === 'Enter') {
-    e.preventDefault();
+
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    }
   }
-}
+
 
 
   return (
@@ -98,49 +105,49 @@ const handleKeyPress = (e) => {
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor='name'>Name :</label>
-                          <input type='name' id='name' name='name' ></input>
+                          <input type='name' id='name' name='name' required></input>
                         </td>
                       </tr>
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor="calories">Calories :</label>
-                          <input type="calories" id="calories" name="calories" ></input>
+                          <input type="calories" id="calories" name="calories" required></input>
                         </td>
                       </tr>
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor="carbohydrates">Carbohydrates :</label>
-                          <input type="carbohydrates" id="carbohydrates" name="carbohydrates"  ></input>
+                          <input type="carbohydrates" id="carbohydrates" name="carbohydrates" required></input>
                         </td>
                       </tr>
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor="protein">Protein :</label>
-                          <input type="protein" id="protein" name="protein"  ></input>
+                          <input type="protein" id="protein" name="protein" required></input>
                         </td>
                       </tr>
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor="fat">Fat :</label>
-                          <input type="fat" id="fat" name="fat" ></input>
+                          <input type="fat" id="fat" name="fat" required></input>
                         </td>
                       </tr>
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor="meat">Meat :</label>
-                          <input type="radio" id="meat" name="mainDish" value="yes" checked={mainDish.meat === 'yes'} onChange={onOptionChange('meat')} className="radio-button"></input>
+                          <input type="radio" id="meat" name="mainDish" value="meat" checked={mainDish === 'meat'} onChange={onOptionChange} className="radio-button" required></input>
                         </td>
                       </tr>
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor="vegetarian">Vegetarian :</label>
-                          <input type="radio" id="vegetarian" name="mainDish" value="yes" checked={mainDish.vegetarian === 'yes'} onChange={onOptionChange('vegetarian')} className="radio-button"></input>
+                          <input type="radio" id="vegetarian" name="mainDish" value="vegetarian" checked={mainDish === 'vegetarian'} onChange={onOptionChange} className="radio-button" required></input>
                         </td>
                       </tr>
                       <tr>
                         <td>
                           <label className={styles.label} htmlFor="vegan">Vegan :</label>
-                          <input type="radio" id="vegan" name="mainDish" value="yes" checked={mainDish.vegan === 'yes'} onChange={onOptionChange('vegan')} className="radio-button"></input>
+                          <input type="radio" id="vegan" name="mainDish" value="vegan" checked={mainDish === 'vegan'} onChange={onOptionChange} className="radio-button" required></input>
                         </td>
                       </tr>
                     </tbody>

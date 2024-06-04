224971b810854ca9e8981708615d0355ba58cc1a
erbiit03 <erbiit03@hs-esslingen.de>
2024-06-04 15:12:28 +0200

    added data in database

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,16 +2,10 @@
 import { getSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
-<<<<<<< HEAD
 import Header from "@/components/Header/Header"
 import DashboardBox from "@/components/DashboardBox/DashboardBox"
 import FeatureButtons from "@/components/FeatureButtons/FeatureButtons"
 import Footer from "@/components/Footer/Footer"
-=======
-import LogoutButton from "@/components/Logout/LogoutButton"
-import Navbar from "@/components/Navbar/Navbar"
-import Link from "next/link"
->>>>>>> feature-summary
 
 const Dashboard = () => {
   const [session, setSession] = useState()
@@ -32,19 +26,10 @@ const Dashboard = () => {
 
   return (
     <div>
-<<<<<<< HEAD
       <Header />
       <DashboardBox />
       <FeatureButtons />
       <Footer />
-=======
-      <Navbar />
-      <div>You are logged in :3</div>
-      <Link href="/dashboard/summary">
-        <button>Summary Page</button>
-      </Link>
-      <LogoutButton />
->>>>>>> feature-summary
     </div>
   )
 }
